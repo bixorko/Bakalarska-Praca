2c499f31217a992bd5fce5210749a8f47157c3e3
bixorko <xvinar00@stud.fit.vutbr.cz>
2021-05-09 18:32:21 +0200

    edited configure.sh comments

--- configure.sh
+++ configure.sh
@@ -1,13 +1,19 @@
-# configure.sh
-# script created by Peter Vinarcik
-# xvinar00
-# xvinar00@stud.fit.vutbr.cz
-# this script automatically install camera software into raspberry pi...
-# precalibrated for Waveshare RPi Camera (M)
-# otherwise user have to create calibration images and send it to Bakalarska-praca/rpos/python/calibImgs
+#####################################################################
+# 								configure.sh						#
+# 					script created by Peter Vinarcik				#
+# 								  xvinar00							#
+# 						xvinar00@stud.fit.vutbr.cz					#
+# this script automatically install camera software 				#
+# into raspberry pi...												#
+# precalibrated for Waveshare RPi Camera (M)						#
+# otherwise user have to create calibration images 					#
+#and send it to Bakalarska-praca/rpos/python/calibImgs				#
+#####################################################################
 
 #!/usr/bin/bash
+# if rpos is not in folder
 git clone https://www.github.com/bixorko/rpos
+# install dependecies based on RPOS github
 sudo apt-get install npm
 sudo npm install -g npm@latest
 sudo apt install git gstreamer1.0-plugins-bad gstreamer1.0-plugins-base \
@@ -16,28 +22,27 @@ sudo apt install git gstreamer1.0-plugins-bad gstreamer1.0-plugins-base \
  libgstreamer1.0-0 gstreamer1.0-omx \
  libgstreamer-plugins-base1.0-dev gtk-doc-tools
 sudo apt-get install python-gi gir1.2-gst-plugins-base-1.0 gir1.2-gst-rtsp-server-1.0
+# install rpicamsrc for GStreamer
 git clone https://github.com/thaytan/gst-rpicamsrc.git
 cd gst-rpicamsrc
 ./autogen.sh
-#echo "WRITING MAKE"
 make
-#echo "WRITED MAKE"
-#echo "WRITING INSTALL"
 sudo make install
-#echo "WRITED INSTALL"
 cd ..
+# install RTSP Server for GStreamer
 git clone git://anongit.freedesktop.org/gstreamer/gst-rtsp-server
 cd gst-rtsp-server
 git checkout 1.4.5
 ./autogen.sh
-#echo "WRITING MAKE"
 make
 sudo make install
 cd ~
+# install Shinobi
 sudo su <<EOF
 bash <(curl -s https://gitlab.com/Shinobi-Systems/Shinobi-Installer/raw/master/shinobi-install.sh)
 bash <(curl -s https://gitlab.com/Shinobi-Systems/Shinobi-Installer/raw/master/shinobi-install.sh)
 1
 EOF
+# run camera
 cd ~/Bakalarska-praca/rpos
 node rpos.js
